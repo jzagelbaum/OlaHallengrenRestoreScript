a1a0c232dbafc4a21f0a92b7299d49fd26e23d44
Orestes <MisterZeus@users.noreply.github.com>
2023-06-20 16:42:44 +0100

    Update RestoreCommand.sql

--- RestoreCommand.sql
+++ RestoreCommand.sql
@@ -1,25 +1,11 @@
-
-IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RestoreCommand]') AND type in (N'P', N'PC'))
-DROP PROCEDURE [dbo].[RestoreCommand]
+DROP PROCEDURE IF EXISTS dbo.RestoreCommand
 GO
-
 SET ANSI_NULLS ON
-GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[RestoreCommand]') AND type in (N'P', N'PC'))
-BEGIN
-EXEC dbo.sp_executesql @statement = N'CREATE PROCEDURE [dbo].[RestoreCommand] AS' 
-END
-GO
-
-
-
-ALTER PROCEDURE [dbo].[RestoreCommand] AS
-
-/*************************************************************************************************************
+CREATE PROCEDURE dbo.RestoreCommand AS
+/*
+*********************************************************************************************
 Script for creating automated restore scripts based on Ola Hallengren's Maintenance Solution. 
 Source: https://ola.hallengren.com
 
@@ -38,14 +24,17 @@ in order to avoid data loss (also remove any replace options for full backups).
 Make sure sql agent has read / write to the directory that you want the restore script created.
 
 Script will read backup file location from @Directory value used in respective DatabaseBackup job (NULL is supported). 
-Set @LogToTable = 'Y' for all backup jobs! (This is the defaut).  
+SET @LogToTable = 'Y' for all backup jobs! (This is the default).  
 
-Created by Jared Zagelbaum, 4/13/2015, https://jaredzagelbaum.wordpress.com/
+Created by Jared Zagelbaum, 2015-04-13, https://jaredzagelbaum.wordpress.com/
 For intro / tutorial see: https://jaredzagelbaum.wordpress.com/2015/04/16/automated-restore-script-output-for-ola-hallengrens-maintenance-solution/
 Follow me on Twitter!: @JaredZagelbaum
-
-**************************************************************************************************************/
-
+*********************************************************************************************
+2019-09-13 - Lee Fisher - Added functionality to support Blob Storage as a backup location
+*********************************************************************************************
+2023-06-20 - Fixed problems when deploying to case-sensitive SQL Instance.
+*********************************************************************************************
+*/
 
 SET NOCOUNT ON
 
@@ -54,141 +43,175 @@ Declare @DatabaseNamePartition sysname = 'N/A'
 Declare @Command nvarchar(max)
 Declare @IncludeCopyOnly nvarchar(max) = 'Y'   -- include copy only backups in restore script? Added for AlwaysOn support
 Declare @message nvarchar(max)
-Declare restorecursor CURSOR FAST_FORWARD FOR
+Declare restorecursor CURSOR FAST_FORWARD
+	FOR
+	with completed_ola_backups AS (
+		SELECT  [ID]
+		  ,[DatabaseName]
+		  ,[SchemaName]
+		  ,[ObjectName]
+		  ,[ObjectType]
+		  ,[IndexName]
+		  ,[IndexType]
+		  ,[StatisticsName]
+		  ,[PartitionNumber]
+		  ,[ExtendedInfo]
+		  ,[Command]
+		  ,[CommandType]
+		  ,[StartTime]
+		  ,[EndTime]
+		  ,[ErrorNumber]
+		  ,[ErrorMessage]
+		  /***************************************************************************
+		  -- Identify if Disk or URL backup location
+		  ****************************************************************************/
+		  -- Identify if disk or url location 
+		  , CASE WHEN [Command] LIKE '%TO URL%' THEN 'URL' ELSE 'DISK'
+			END Location
+		   /******************************************************************************
+			Expanded to include forward slash(/) used in URL path	   
+		   *******************************************************************************/
+		  ,CASE WHEN [Command] LIKE '%\LOG\%' THEN 'Log'
+				WHEN [Command] LIKE '%/LOG/%' THEN 'Log'  -- Added for URL
+				WHEN @IncludeCopyOnly = 'Y' AND [Command] LIKE '%\LOG_COPY_ONLY\%' THEN 'Log'
+				WHEN @IncludeCopyOnly = 'Y' AND [Command] LIKE '%/LOG_COPY_ONLY/%' THEN 'Log'  -- Added for URL
+				WHEN [Command] LIKE '%\DIFF\%' THEN 'Diff'
+				WHEN [Command] LIKE '%/DIFF/%' THEN 'Diff'	-- Added for URL
+				WHEN [Command] LIKE '%\FULL\%' THEN 'Full'
+				WHEN [Command] LIKE '%/FULL/%' THEN 'Full'	-- Added for URL
+				WHEN @IncludeCopyOnly = 'Y' AND [Command] LIKE '%\FULL_COPY_ONLY\%' THEN 'Full'
+				WHEN @IncludeCopyOnly = 'Y' AND [Command] LIKE '%/FULL_COPY_ONLY/%' THEN 'Full' -- Added for URL
+		  End BackupType
+		   /******************************************************************************
+			Expanded to include forward slash(/) used in URL path	   
+		   *******************************************************************************/
+		  ,CASE WHEN [Command] LIKE '%\LOG\%' THEN 3
+				WHEN [Command] LIKE '%/LOG/%' THEN 3 -- Added for URL
+				WHEN @IncludeCopyOnly = 'Y' AND [Command] LIKE '%\LOG_COPY_ONLY\%' THEN 3
+				WHEN @IncludeCopyOnly = 'Y' AND [Command] LIKE '%/LOG_COPY_ONLY/%' THEN 3 -- Added for URL
+				WHEN [Command] LIKE '%\DIFF\%' THEN 2
+				WHEN [Command] LIKE '%/DIFF/%' THEN 2 -- Added for URL
+				WHEN [Command] LIKE '%\FULL\%' THEN 1
+				WHEN [Command] LIKE '%/FULL/%' THEN 1 -- Added for URL
+				WHEN @IncludeCopyOnly = 'Y' AND [Command] LIKE '%\FULL_COPY_ONLY\%' THEN 1
+				WHEN @IncludeCopyOnly = 'Y' AND [Command] LIKE '%/FULL_COPY_ONLY/%' THEN 1 -- Added for URL
+		  End BackupTypeOrder
+		  ,CASE CommandType
+			WHEN 'BACKUP_LOG'
+				THEN CHARINDEX('.trn', Command)
+			WHEN 'BACKUP_DATABASE'
+				THEN CHARINDEX('.bak', Command)
+			END filechar
+		   /******************************************************************************
+			Get BLOCKSIZE and MAXTRANSFERSIZE IF EXISTS for Azure Blob Storage
+		   *******************************************************************************/
+			, ( SELECT  STUFF((SELECT  ',' + value
+				from String_Split([Command], ',')
+				where value like '%BLOCKSIZE%' or value like '%MAXTRANSFERSIZE%'
+						FOR XML PATH('')), 1, 1, '') ) AzureBlobStorage
+	  FROM [dbo].[CommandLog]
+	  WHERE CommandType IN ('BACKUP_LOG', 'BACKUP_DATABASE')
+	  AND EndTime IS NOT NULL -- Completed Backups Only
+	  AND ErrorNumber = 0
+	  )
+	  ,lastfull as
+	  (
+	  SELECT MAX( [ID]) FullId
+	  ,DatabaseName
+	  FROM completed_ola_backups
+	  WHERE BackupType = 'Full'
+	  GROUP BY DatabaseName
+	  )
+	  ,lastdiff as
+	 (
+	  SELECT MAX( [ID]) DiffId
+	  ,cob.DatabaseName
+	  FROM completed_ola_backups cob
+	  INNER JOIN lastfull lf
+	  ON cob.DatabaseName = lf.DatabaseName
+	  AND cob.[ID] > lf.FullId
+	  WHERE BackupType = 'Diff'
+	  GROUP BY cob.DatabaseName
+	  )
+	  ,lastnonlog as
+	  (
+	  SELECT Max([Id]) LogIdBoundary
+	  ,DatabaseName
+	  FROM 
+		(
+			SELECT FullId Id, DatabaseName
+			FROM lastfull
+			UNION ALL
+			SELECT DiffId Id, ld.DatabaseName
+			FROM lastdiff ld
+		) Nonlog
+	  GROUP BY DatabaseName
+	  )
+	  ,lastlogs as
+	  (
+	  SELECT cob.[ID] logid
+	  FROM completed_ola_backups cob
+	  INNER JOIN lastnonlog lnl
+	  ON cob.DatabaseName = lnl.DatabaseName
+	  AND cob.[ID] > lnl.LogIdBoundary
+	 )
+	 ,validbackups as
+	 (
+	 SELECT FullId backupid
+	 FROM lastfull
+	 UNION
+	 SELECT DiffId backupid
+	 FROM lastdiff
+	 UNION
+	 SELECT logid backupid
+	 FROM lastlogs
+	 )
 
-with completed_ola_backups as
-(
-SELECT  [ID]
-      ,[DatabaseName]
-      ,[SchemaName]
-      ,[ObjectName]
-      ,[ObjectType]
-      ,[IndexName]
-      ,[IndexType]
-      ,[StatisticsName]
-      ,[PartitionNumber]
-      ,[ExtendedInfo]
-      ,[Command]
-      ,[CommandType]
-      ,[StartTime]
-      ,[EndTime]
-      ,[ErrorNumber]
-      ,[ErrorMessage]
-	  ,CASE WHEN [Command] LIKE '%\LOG\%' THEN 'Log'
-	  WHEN @IncludeCopyOnly = 'Y' AND [Command] LIKE '%\LOG_COPY_ONLY\%' THEN 'Log'
-	  WHEN [Command] LIKE '%\DIFF\%' THEN 'Diff'
-	  WHEN [Command] LIKE '%\FULL\%' THEN 'Full'
-	  WHEN @IncludeCopyOnly = 'Y' AND [Command] LIKE '%\FULL_COPY_ONLY\%' THEN 'Full'
-	  End BackupType
-	  ,CASE WHEN [Command] LIKE '%\LOG\%' THEN 3
-	   WHEN @IncludeCopyOnly = 'Y' AND [Command] LIKE '%\LOG_COPY_ONLY\%' THEN 3
-	  WHEN [Command] LIKE '%\DIFF\%' THEN 2
-	  WHEN [Command] LIKE '%\FULL\%' THEN 1
-	   WHEN @IncludeCopyOnly = 'Y' AND [Command] LIKE '%\FULL_COPY_ONLY\%' THEN 1
-	  End BackupTypeOrder
-	  ,CASE CommandType
-	WHEN 'BACKUP_LOG'
-	THEN CHARINDEX('.trn', Command)
-	WHEN 'BACKUP_DATABASE'
-	THEN CHARINDEX('.bak', Command)
-	END filechar
-  FROM [dbo].[CommandLog]
-  WHERE CommandType IN ('BACKUP_LOG', 'BACKUP_DATABASE')
-  AND EndTime IS NOT NULL -- Completed Backups Only
-  AND ErrorNumber = 0
-  )
-  ,lastfull as
-  (
-  SELECT MAX( [id]) FullId
-  ,DatabaseName
-  FROM completed_ola_backups
-  WHERE BackupType = 'Full'
-  GROUP BY DatabaseName
-  )
-  ,lastdiff as
- (
-  SELECT MAX( [id]) DiffId
-  ,cob.DatabaseName
-  FROM completed_ola_backups cob
-  INNER JOIN lastfull lf
-  ON cob.DatabaseName = lf.DatabaseName
-  AND cob.[ID] > lf.FullId
-  WHERE BackupType = 'Diff'
-  GROUP BY cob.DatabaseName
-  )
-  ,lastnonlog as
-  (
-  SELECT Max([Id]) LogIdBoundary
-  ,DatabaseName
-  FROM 
-	(
-		SELECT Fullid Id, DatabaseName
-		FROM lastfull
-		UNION ALL
-		SELECT DiffId Id, ld.DatabaseName
-		FROM lastdiff ld
-	) Nonlog
-  GROUP BY DatabaseName
-  )
-  ,lastlogs as
-  (
-  SELECT cob.[Id] logid
-  FROM completed_ola_backups cob
-  INNER JOIN lastnonlog lnl
-  ON cob.DatabaseName = lnl.DatabaseName
-  AND cob.[ID] > lnl.LogIdBoundary
- )
- ,validbackups as
- (
- SELECT FullId backupid
- FROM lastfull
- UNION
- SELECT DiffId backupid
- FROM lastdiff
- UNION
- SELECT logid backupid
- FROM lastlogs
- )
-
- SELECT cob.DatabaseName
- , 
-			Replace(
-			Replace(
-			Replace( 
-			LEFT(Command, filechar + 3) 
-		, 'BACKUP LOG', 'RESTORE LOG')
-		, 'BACKUP DATABASE', 'RESTORE DATABASE')
-		, 'TO DISK', 'FROM DISK') + ''' WITH NORECOVERY' 
-		+ CASE BackupType
-	WHEN 'Full'
-	THEN ', REPLACE;'
-	ELSE ';'
-	END RestoreCommand
+	 SELECT cob.DatabaseName
+	 , 
+				Replace(
+				Replace(
+				Replace( 
+				REPLACE(
+				LEFT(Command, filechar + 3) 
+			, 'BACKUP LOG', 'RESTORE LOG')
+			, 'BACKUP DATABASE', 'RESTORE DATABASE')
+			, 'TO DISK', 'FROM DISK')
+			-- ADDED FOR URL
+			, 'TO URL', 'FROM URL') + ''' WITH NORECOVERY, STATS=5' 
+			+ CASE WHEN AzureBlobStorage is null then ''    --- Support Blob Storage
+			   ELSE ', ' + AzureBlobStorage
+			   END
+			+ CASE BackupType
+		WHEN 'Full'
+		THEN ', REPLACE;'
+		ELSE ';'
+		END RestoreCommand
  
- FROM completed_ola_backups cob
- WHERE EXISTS
-	(SELECT *
-	FROM validbackups vb
-	WHERE cob.[ID] = vb.backupid
-	)
- ORDER BY cob.DatabaseName, Id, BackupTypeOrder
- ;
+	 FROM completed_ola_backups cob
+	 WHERE EXISTS
+		(SELECT *
+		FROM validbackups vb
+		WHERE cob.[ID] = vb.backupid
+		)
+	 ORDER BY cob.DatabaseName, ID, BackupTypeOrder
+	 ;
 
 
  RAISERROR( '/*****************************************************************', 10, 1) WITH NOWAIT
- set @message = 'Emergency Script Restore for ' + @@Servername +  CASE @@Servicename WHEN 'MSSQLSERVER' THEN '' ELSE '\' + @@Servicename END 
+ SET @message = 'Emergency Script Restore for ' + @@Servername +  CASE @@Servicename WHEN 'MSSQLSERVER' THEN '' ELSE '\' + @@Servicename END 
  RAISERROR(@message,10,1) WITH NOWAIT
- set @message = 'Generated ' + convert(nvarchar, getdate(), 9)  
+ SET @message = 'Generated ' + convert(nvarchar, getdate(), 9)  
  RAISERROR(@message,10,1) WITH NOWAIT
- set @message = 'Script does not perform a tail log backup. Dataloss may occur, use only for emergency DR.'
+ SET @message = 'Script does not perform a tail log backup. Dataloss may occur, use only for emergency DR.'
  RAISERROR(@message,10,1) WITH NOWAIT
  RAISERROR( '******************************************************************/', 10, 1) WITH NOWAIT
 
 
- OPEN RestoreCursor
+ OPEN restorecursor
 
  FETCH NEXT FROM restorecursor
- INTO @databasename, @command
+ INTO @DatabaseName, @Command
 
 
  WHILE @@FETCH_STATUS = 0
@@ -196,13 +219,13 @@ SELECT  [ID]
 
  IF @DatabaseName <> @DatabaseNamePartition AND @DatabaseNamePartition <> 'N/A'
 	BEGIN
-	set @message = 'RESTORE DATABASE ' + '[' + @DatabaseNamePartition + ']' + ' WITH RECOVERY;'
+	SET @message = 'RESTORE DATABASE ' + '[' + @DatabaseNamePartition + ']' + ' WITH RECOVERY;'
 	RAISERROR(@message,10,1) WITH NOWAIT
 	END
 
 	IF @DatabaseName <> @DatabaseNamePartition
 		BEGIN
-			set @message = char(13) + char(10) + char(13) + char(10) + '--------' + @DatabaseName + '-------------'
+			SET @message = char(13) + char(10) + char(13) + char(10) + '--------' + @DatabaseName + '-------------'
 			RAISERROR(@message,10,1) WITH NOWAIT
 		END
 	
@@ -212,22 +235,17 @@ SELECT  [ID]
 
 	SET @DatabaseNamePartition = @DatabaseName
 FETCH NEXT FROM restorecursor
-INTO @databasename, @command
+INTO @DatabaseName, @Command
 
 END
 
-set @message =  'RESTORE DATABASE ' + '[' +  @DatabaseNamePartition + ']' + ' WITH RECOVERY;' 
+SET @message =  'RESTORE DATABASE ' + '[' +  @DatabaseNamePartition + ']' + ' WITH RECOVERY;' 
 RAISERROR(@message,10,1) WITH NOWAIT
 ;
 
 CLOSE restorecursor;
 DEALLOCATE restorecursor;
-
-
-
-
-
-
 GO
 
-
+--test the RestoreCommand sproc!
+EXEC dbo.RestoreCommand
